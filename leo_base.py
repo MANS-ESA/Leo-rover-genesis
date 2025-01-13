592be75945d680ddaa8ab0f83c7d6a3f4d0a0538
Julien Legrand <julienlegrand@Julien--MacBook-Pro.local>
2025-01-13 12:17:32 +0100

    feat: add leo bazse

--- leo_base.py
+++ leo_base.py
@@ -1,24 +1,13 @@
 import genesis as gs
+gs.init(backend=gs.cuda)
 
-gs.init(backend=gs.metal)
-
-scene = gs.Scene()
-
-plane = scene.add_entity(
-    gs.morphs.Plane(),
-)
+scene = gs.Scene(show_viewer=True)
+plane = scene.add_entity(gs.morphs.Plane())
 franka = scene.add_entity(
-    # gs.morphs.URDF(
-    #     file='urdf/panda_bullet/panda.urdf',
-    #     fixed=True,
-    # ),
-    gs.morphs.URDF(file="./leo_sim.urdf"),
+    gs.morphs.URDF(file='./leo_sim.urdf'),
 )
-
-def run_sim(scene): 
-    for i in range(1000):
-        scene.step()
 
 scene.build()
-gs.tools.run_in_another_thread(fn=run_sim, args=(scene, ))
-scene.viewer.start()
+
+for i in range(1000):
+    scene.step()
